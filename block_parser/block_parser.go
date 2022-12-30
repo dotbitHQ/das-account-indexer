442aa80cbea2aaf98e4022d9d313fc5c7b6c31a5
scorpiotzh <835598264@qq.com>
2022-12-30 09:59:39 +0800

    feat: modify checkContractVersion

--- block_parser/block_parser.go
+++ block_parser/block_parser.go
@@ -250,7 +250,7 @@ func (b *BlockParser) checkContractVersion() error {
 			if err == core.ErrContractMajorVersionDiff {
 				log.Errorf("contract[%s] version diff, chain[%s], service[%s].", v, chainVersion, defaultVersion)
 				log.Error("Please update the service. [https://github.com/dotbitHQ/das-account-indexer]")
-				if b.Cancel != nil {
+				if b.Cancel != nil && !config.Cfg.Server.NotExit {
 					b.Cancel()
 				}
 				return err

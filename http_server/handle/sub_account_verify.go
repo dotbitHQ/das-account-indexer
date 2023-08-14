5d384dc5e1dc45a40d10c7c0db4e09df8a4ffc70
michael <457813723@qq.com>
2023-08-14 15:58:51 +0800

    update api code

--- http_server/handle/sub_account_verify.go
+++ http_server/handle/sub_account_verify.go
@@ -1,10 +1,10 @@
 package handle
 
 import (
-	"das-account-indexer/http_server/code"
 	"encoding/json"
 	"fmt"
 	"github.com/dotbitHQ/das-lib/common"
+	code "github.com/dotbitHQ/das-lib/http_api"
 	"github.com/gin-gonic/gin"
 	"github.com/scorpiotzh/toolib"
 	"net/http"
